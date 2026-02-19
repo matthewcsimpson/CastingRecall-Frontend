40053e9c56d0c6b73f155db4666349534bf1e07d
Matthew Simpson <mcsimpson+gh@gmail.com>
2026-02-19 00:50:32 -0800

    kofi

--- src/components/SiteFooter/SiteFooter.jsx
+++ src/components/SiteFooter/SiteFooter.jsx
@@ -17,6 +17,18 @@ const SiteFooter = () => {
           </a>
           .
         </p>
+        <a
+          className="footer__kofi-link"
+          href="https://ko-fi.com/H2H1167GZ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="footer__kofi-image"
+            src="https://storage.ko-fi.com/cdn/kofi3.png?v=6"
+            alt="Buy Me a Coffee at ko-fi.com"
+          />
+        </a>
         <p className="footer__text">
           This site uses the TMDB API, but is not endorsed thereby.
         </p>
